14ed292245db68ecc669c1ff79d10b19899ec3b5
kostialantsov <kanstantsin_lantsov@senla.eu>
2022-12-13 16:33:08 +0300

    initial commit

---
+++ src/main/java/com/company/jooqpetproject/JooqPetProjectApplication.java
@@ -0,0 +1,13 @@
+package com.company.jooqpetproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooqPetProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JooqPetProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/company/jooqpetproject/JooqPetProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.jooqpetproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JooqPetProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
